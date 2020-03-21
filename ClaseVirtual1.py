735c699015d06149ad10084b22c1f335b43cd965
JP Personal-PC <ingejp@outlook.com>
2020-03-21 09:02:04 -0600

    Primera clase virtual, introducción a python

---
+++ ClaseVirtual1.py
@@ -0,0 +1,9 @@
+primerNumero = 10
+segundoNumero =20.5
+
+def sumar():
+    resultadoDeOperacion=primerNumero + segundoNumero
+    return resultadoDeOperacion
+
+print('el resultado es => ' + str(sumar()))
+
